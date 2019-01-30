401a14437532bdd5dab3af232ce42b0b70922b92
Muhammad Asim <asimuaf41@gmail.com>
2019-01-30 14:30:42 +0500

    install express again and remove cors code from app.js

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express=require('express');
 const server=express();
 // Import the library:
-var cors = require('cors');
+
 const bodyParser=require('body-parser');
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  })
-server.use(cors());  
+
 server.use(express.static('./build'));
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json())
