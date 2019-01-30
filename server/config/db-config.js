8ea218624ad86f07565d15e62b39fdb84a840e01
Muhammad Asim <asimuaf41@gmail.com>
2019-01-30 15:07:06 +0500

    remove special character from password

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-var db_url = 'mongodb://Muhammad Asim:Socialstudy41@@ds217125.mlab.com:17125/myresturant'
+var db_url = 'mongodb://Asim:Socialstudy41@ds217125.mlab.com:17125/myresturant'
 mongoose.connect(db_url, { useNewUrlParser: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
